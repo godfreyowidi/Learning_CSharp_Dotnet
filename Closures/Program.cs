49e391ca2de0f08f2c73f8bbbbdf627a5e4dd581
GOdfrey Owidi <godfreyowiidi@gmail.com>
2021-12-15 19:16:37 +0300

    Add Closures and Types.

---
+++ Closures/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+Func<int, int> calculator = CreateCalculator();
+Console.WriteLine(calculator(2));
+
+int CalculateSomething(int n) 
+{
+    if (n == 0) return 0;
+
+    var factor = 42;
+    return factor * CalculateSomething(n - 1);
+}
+
+Func<int, int> CreateCalculator() // Factory pattern
+{
+    var factor = 42;
+    return n => n * factor;
+}
+
+Bestfriends CreateCalulatorInternal()
+{
+    return new Bestfriends { factor = 42 };
+}
+class Bestfriends
+{
+    public int factor;
+    public int Calculator(int n) => n * factor;
+}
---
+++ TypeSystem/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TypeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myname = "Godfrey";
+            myname = myname.ToUpper();
+            Console.WriteLine(myname);
+
+            // Boxing
+            object o = 42;
+            object o2 = 42;
+            if (o == o2) 
+            {
+                Console.WriteLine("Same!");
+            }
+
+            if(o.Equals(42))
+            {
+                Console.WriteLine("Same Here!");
+            }
+
+            // Unboxing
+            var n = (int)o; // Typecasting
+
+            int myNumber = 42;
+            // myNumber = "FooBar";
+
+            dynamic myDynamic = 32;
+            myDynamic = "FooBar";
+
+            // Struct or class
+
+            void DoSomething();
+            {
+                var dt = new DateTime();
+                var dt2 = dt;
+
+                var sb = new StringBuilder();
+                var sb2 = sb;
+            }
+
+            // gabbage collector -> monitors the memory - finalizers
+            // GC.Collect(); relevant to reference types
+        }
+    }
+}
